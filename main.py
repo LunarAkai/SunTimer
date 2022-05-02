f26874a83fb01600cd154c3c3f20b9c56556cf96
LunarAkai <oddeys99@gmail.com>
2022-05-02 16:39:10 +0200

    added option for different cities

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 import json
 
-# Erfurt, GER, https://www.latlong.net/place/erfurt-thuringia-germany-29863.html
-lat = 50.983334
-lng = 11.033333
 
-url = f"https://api.sunrise-sunset.org/json?lat=,{lat},&lng=,{lng},&date=today"  # https://sunrise-sunset.org/api
+with open("config.json") as configFile:
+    jsonObject = json.load(configFile)
+    configFile.close()
+
+    apiKey = jsonObject['keys']['api_key']
 
 
 def requestAPIResp(_url):
@@ -14,7 +15,13 @@ def requestAPIResp(_url):
 
 
 def saveToJSON(_apiResponse):
-    file = open("suntimerfile.json", "w")
+    global fileName
+    if isCityAPI:
+        fileName = "currentcity.json"
+    if not isCityAPI:
+        fileName = "suntimerfile.json"
+
+    file = open(fileName, "w")
     json.dump(res.json(), file, indent=6)
     file.close()
 
@@ -22,23 +29,50 @@ def saveToJSON(_apiResponse):
 #####################################################
 
 
-res = requestAPIResp(url)
+limit = 1
+
+cityName = input("Please enter the city name: ")
+countryCode = input("Please enter the country code: ")
+
+urlCity = f"http://api.openweathermap.org/geo/1.0/direct?q={cityName},{countryCode}&limit={limit}&appid={apiKey}"
+isCityAPI = True
+
+res = requestAPIResp(urlCity)
 
 if res.status_code == 200:
+
     saveToJSON(res)
+    isCityAPI = False
 
-    with open("suntimerfile.json") as jsonFile:
-        jsonObject = json.load(jsonFile)
-        jsonFile.close()
+    with open("currentcity.json", "r") as jsonCityFile:
+        obj = json.load(jsonCityFile)
+        jsonCityFile.close()
 
-        sunrise = jsonObject['results']['sunrise']
-        sunset = jsonObject['results']['sunset']
-        solar_noon = jsonObject['results']['solar_noon']
-        day_length = jsonObject['results']['day_length']
+        lat = obj[0]['lat']
+        lng = obj[0]['lon']
 
-        print("Sunrise: " + sunrise, "Sunset: " + sunset, "Solar Noon:" + solar_noon, "Day Length: " + day_length,
-              sep=" , ")
-        print("Times are in UTC")
+    urlSun = f"https://api.sunrise-sunset.org/json?lat=,{lat},&lng=,{lng},&date=today"  # https://sunrise-sunset.org/api
+    print(urlSun)
+    res = requestAPIResp(urlSun)
+    print(res.status_code)
+
+    if res.status_code == 200:
+        saveToJSON(res)
+
+        with open("suntimerfile.json") as jsonFile:
+            jsonObject = json.load(jsonFile)
+            jsonFile.close()
+
+            sunrise = jsonObject['results']['sunrise']
+            sunset = jsonObject['results']['sunset']
+            solarNoon = jsonObject['results']['solar_noon']
+            dayLength = jsonObject['results']['day_length']
+
+            print("Sunrise: " + sunrise, "Sunset: " + sunset, "Solar Noon: " + solarNoon, "Day Length: " + dayLength,
+                  sep=" , ")
+            print("Times are in UTC")
+    else:
+        print("No valid Sun Response")
 
 else:
-    print("No valid response")
+    print("No valid City response")
